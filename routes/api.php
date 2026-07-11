97c8c8367a3badd86af144e8b97c00f904ef0399
MmGrand <maksimzarubin20002000@gmail.com>
2026-07-11 13:08:54 +0300

    feat: добавил контроллер для управления пользователями

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/users', [\App\Http\Controllers\api\UserController::class, 'index']);
+Route::get('/users/{user}', [\App\Http\Controllers\api\UserController::class, 'show']);
+Route::put('/users/{user}', [\App\Http\Controllers\api\UserController::class, 'update']);
