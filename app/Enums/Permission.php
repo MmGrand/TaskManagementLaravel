8983b769f7324cb8360f940d1e47a8f9cb0e2e71
MmGrand <maksimzarubin20002000@gmail.com>
2026-07-12 13:52:46 +0300

    feat: добавлен enum для прав

---
+++ app/Enums/Permission.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Enums;
+
+enum Permission: string
+{
+    case ProjectsViewAny = 'projects.viewAny';
+    case ProjectsView = 'projects.view';
+    case ProjectsCreate = 'projects.create';
+    case ProjectsUpdate = 'projects.update';
+    case ProjectsDelete = 'projects.delete';
+
+    case TasksViewAny = 'tasks.viewAny';
+    case TasksView = 'tasks.view';
+    case TasksCreate = 'tasks.create';
+    case TasksUpdate = 'tasks.update';
+    case TasksDelete = 'tasks.delete';
+
+    case UsersViewAny = 'users.viewAny';
+    case UsersView = 'users.view';
+    case UsersUpdate = 'users.update';
+}
