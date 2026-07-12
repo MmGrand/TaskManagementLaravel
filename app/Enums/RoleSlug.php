94a9ce714834a3cf913dd3f85147f91ec2bae50c
MmGrand <maksimzarubin20002000@gmail.com>
2026-07-12 13:54:42 +0300

    feat: добавлен enum RoleSlug с ролями

---
+++ app/Enums/RoleSlug.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Enums;
+
+enum RoleSlug: string
+{
+    case Admin = 'admin';
+    case Manager = 'manager';
+    case User = 'user';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Admin => 'Администратор',
+            self::Manager => 'Менеджер',
+            self::User => 'Пользователь',
+        };
+    }
+
+    public function defaultPermissions(): array
+    {
+        $toValues = fn (array $permissions): array => array_map(
+            fn (Permission $permission): string => $permission->value,
+            $permissions,
+        );
+
+        return match ($this) {
+            self::Admin => ['*'],
+            self::Manager => $toValues([
+                Permission::ProjectsViewAny,
+                Permission::ProjectsView,
+                Permission::ProjectsCreate,
+                Permission::ProjectsUpdate,
+                Permission::ProjectsDelete,
+                Permission::TasksViewAny,
+                Permission::TasksView,
+                Permission::TasksCreate,
+                Permission::TasksUpdate,
+                Permission::TasksDelete,
+                Permission::UsersViewAny,
+                Permission::UsersView,
+            ]),
+            self::User => $toValues([
+                Permission::ProjectsViewAny,
+                Permission::ProjectsView,
+                Permission::TasksViewAny,
+                Permission::TasksView,
+                Permission::TasksUpdate,
+            ]),
+        };
+    }
+}
