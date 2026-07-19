4dd3e11db540a896487f9c8d4540ab8d74ceca70
MmGrand <maksimzarubin20002000@gmail.com>
2026-07-19 15:16:12 +0300

    feat: поправил контроллер для проектов

--- app/Http/Controllers/api/ProjectController.php
+++ app/Http/Controllers/api/ProjectController.php
@@ -2,20 +2,28 @@
 
 namespace App\Http\Controllers\api;
 
+use App\Filters\ProjectFilter;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\IndexProjectRequest;
+use App\Http\Resources\ProjectResource;
 use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index()
+    public function index(IndexProjectRequest $request, ProjectFilter $filter)
     {
-        $projects = Project::with('creator')->get();
+        $projects = Project::query()
+            ->with('creator')
+            ->filter($filter)
+            ->latest()
+            ->paginate(15)
+            ->withQueryString();
 
-        return response()->json($projects);
+        return ProjectResource::collection($projects);
     }
 
-    public function create(Request $request)
+    public function store(Request $request)
     {
         $this->authorize('create', Project::class);
 
@@ -27,12 +35,16 @@ class ProjectController extends Controller
 
         $project = $request->user()->projects()->create($validated);
 
-        return response()->json($project, 201);
+        return ProjectResource::make($project->load('creator'))
+            ->response()
+            ->setStatusCode(201);
     }
 
     public function show(Project $project)
     {
-        return response()->json($project);
+        $this->authorize('view', $project);
+
+        return ProjectResource::make($project->load('creator'));
     }
 
     public function update(Request $request, Project $project)
@@ -47,7 +59,7 @@ class ProjectController extends Controller
 
         $project->update($validated);
 
-        return response()->json($project);
+        return ProjectResource::make($project->load('creator'));
     }
 
     public function destroy(Project $project)
